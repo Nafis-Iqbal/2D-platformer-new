22621a35222b2eabbd93e5cb6e41f04d24aa867e
dar1us_7 <rejuanulhuq@gmail.com>
2022-12-22 17:06:16 +0600

    some object

---
+++ 2D-platformer/Assets/ScriptRiz/Enemy1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+    public GameObject isGround;
+    public LayerMask environmentMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
