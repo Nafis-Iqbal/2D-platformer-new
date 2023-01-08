8fdb641795f00567bfb1104f0f8b11c1e606b93f
Jahangir Alam Rocky <x00jahangir@gmail.com>
2023-01-08 18:40:12 +0600

    Player damage system added

---
+++ 2D-platformer/Assets/GMTK/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+    [SerializeField] private Slider playerHealthSlider;
+    [SerializeField] private float health = 100f;
+    [SerializeField] private float healthUIUpdateDuration = 2f;
+
+    private float normalizedHealth = 1f;
+    private float maxHealth = 100f;
+
+    public void OnHitBySword(float damage) {
+        health -= damage;
+        normalizedHealth = health / maxHealth;
+        DOTween.To(() => playerHealthSlider.value, x => playerHealthSlider.value = x, normalizedHealth, healthUIUpdateDuration);
+        if (health <= 0f) {
+            health = 0f;
+            normalizedHealth = 0f;
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ 2D-platformer/Assets/Scripts/EnemySword.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySword : MonoBehaviour {
+    private PlayerHealth playerHealth;
+    [SerializeField] private float swordDamage = 5f;
+
+    private void Awake() {
+        playerHealth = GameManager.Instance.playerTransform.GetComponent<PlayerHealth>();
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            playerHealth.OnHitBySword(swordDamage);
+        }
+    }
+}
