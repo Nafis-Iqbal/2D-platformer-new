914d09e24bb683ca617cbe71d5ecefde82331edb
Jahangir Alam Rocky <x00jahangir@gmail.com>
2023-01-25 18:03:35 +0600

    Added basic grappling indicator

---
+++ 2D-platformer/Assets/Scripts/Collider actions/GrappleCollider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleCollider : MonoBehaviour {
+    [SerializeField] private SpriteRenderer grappleSpriteRenderer;
+    [SerializeField] private Transform grappleSpriteTransform;
+    [SerializeField] private Color grappleActiveColor;
+    [SerializeField] private Color grappleInactiveColor;
+    [SerializeField] private bool grappleActive;
+    [SerializeField] private Vector3 grappleActiveScale;
+    [SerializeField] private float grappleActiveScaleSpeed;
+
+    private void Update() {
+        if (grappleActive) {
+            grappleSpriteTransform.localScale = Vector3.Lerp(grappleSpriteTransform.localScale, grappleActiveScale, Time.deltaTime * grappleActiveScaleSpeed);
+        } else {
+            grappleSpriteTransform.localScale = Vector3.Lerp(grappleSpriteTransform.localScale, Vector3.one, Time.deltaTime * grappleActiveScaleSpeed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            GrappleActivate();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            GrappleInactive();
+        }
+    }
+
+    private void GrappleInactive() {
+        grappleSpriteRenderer.color = grappleInactiveColor;
+        grappleActive = false;
+    }
+
+    private void GrappleActivate() {
+        grappleSpriteRenderer.color = grappleActiveColor;
+        grappleActive = true;
+    }
+}
